548cd278c0efba1da985d698d3868c4f5a2e8268
e1q23079 <e1q23079@oit.ac.jp>
2025-08-16 21:17:29 +0900

    aboutを編集

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Aboutのページです．</h1>
+    <div class="center">
+        <img src='@/assets/icon.png' width='200px' alt='icon'></img>
+        <p className='userName'>Q23079</p>
+        <p className='userMessage'>日々挑戦し続ける，情報系学生の記録</p>
+    </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+.center {
+    min-height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.userName {
+    padding: 5px;
+    font-size: x-large;
+    font-weight: bold;
+}
+
+.userMessage {
+    font-size: calc(8px + 2vmin);
+}
 
 </style>
