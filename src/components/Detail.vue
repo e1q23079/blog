470554c0bd369f77068350405b2b7140dc3f49a3
e1q23079 <e1q23079@oit.ac.jp>
2025-08-16 20:40:34 +0900

    マークダウン対応

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,12 +1,25 @@
 <template>
     <h1>記事:{{ detailId }}</h1>
+    <div v-html="marked(text)"></div>
 </template>
 
 <script setup>
+import { marked } from 'marked';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const detailId = route.params.id;
+
+const text = `
+# h1
+## h2
+### h3
+#### h4
+|a|b|
+|-|-|
+|1|2|
+`
+
 </script>
 
 <style scoped>
