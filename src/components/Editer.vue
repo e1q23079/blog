7d0b8924b5b5da26935a00819bbb506f2e085dc0
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 01:25:12 +0900

    投稿機能のモック

--- src/components/Editer.vue
+++ src/components/Editer.vue
@@ -7,15 +7,38 @@
             <div class="d-flex justify-end">
                 <v-btn-group>
                     <v-btn>取り消し</v-btn>
-                    <v-btn>保存</v-btn>
+                    <v-btn @click="save">公開</v-btn>
                 </v-btn-group>
             </div>
         </v-card-item>
     </v-card>
+
+    <v-dialog v-model="dialog" max-width="400" persistent>
+        <v-card prepend-icon="mdi-check" text="本当に公開しますか？" title="確認">
+            <template v-slot:actions>
+                <v-spacer></v-spacer>
+
+                <v-btn @click="dialog = false">
+                    いいえ
+                </v-btn>
+
+                <v-btn @click="dialog = false">
+                    はい
+                </v-btn>
+            </template>
+        </v-card>
+    </v-dialog>
 </template>
 
 
 <script setup>
+    import { ref } from 'vue'
+
+    const dialog = ref(false);
+
+    const save = () => {
+        dialog.value = true;
+    }
 
 </script>
 
