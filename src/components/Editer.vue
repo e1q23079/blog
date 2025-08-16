9f89f5c1823bc6dbb9abe1e03781fb32b114632c
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 01:15:41 +0900

    保存と取り消し

--- src/components/Editer.vue
+++ src/components/Editer.vue
@@ -4,7 +4,12 @@
             <v-text-field variant="underlined" label="タイトル"></v-text-field>
             <v-card-subtitle class="date">投稿日：2025/08/16</v-card-subtitle>
             <v-textarea label="記事" counter variant="underlined"></v-textarea>
-            <v-btn>保存</v-btn><v-btn>取り消し</v-btn>
+            <div class="d-flex justify-end">
+                <v-btn-group>
+                    <v-btn>取り消し</v-btn>
+                    <v-btn>保存</v-btn>
+                </v-btn-group>
+            </div>
         </v-card-item>
     </v-card>
 </template>
