c657d2bb68ec9bd9533b181ab3496b568186a507
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 15:28:42 +0900

    データベース接続

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -13,13 +13,37 @@
           本文の内容が記述される．
         </v-card-text>
 
+        <h1>テスト{{ blogs }}</h1>
+
+        <br></br>
+
+        <h3 v-for="dt in blogs" :key="dt.id">{{ dt.title }}</h3>
+
+
       </v-card>
 
     </v-col>
   </v-row>
+
+
 </template>
 
-<script>
+<script setup>
+
+  import { ref, onMounted } from 'vue';
+  import { supabase } from '../../utils/supabase';
+
+  const blogs = ref([]);
+
+  async function getBlog() {
+    const { data: data, error } = await supabase.from('blog').select();
+    console.log(data, error);
+    blogs.value = data;
+  }
+
+  onMounted(() => {
+    getBlog();
+  })
 </script>
 
 <style scoped></style>
