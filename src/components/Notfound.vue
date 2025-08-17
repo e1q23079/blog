64fe236dcc8193321be9472bbf742481f9ba441f
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 16:10:40 +0900

    見つかりませんでした

--- src/components/Notfound.vue
+++ src/components/Notfound.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Not Found</h1>
+    <div className="center">
+        <img src=@/assets/icon.png width='200px' alt='icon'></img>
+        <p className='notFound'>Not Found</p>
+        <p className='message'>お探しのページは見つかりませんでした．</p>
+        <a href='/' className='message'>トップ</a>
+    </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+    .center {
+        min-height: 80vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .notFound {
+        padding: 10px;
+        font-size: x-large;
+        font-weight: bold;
+    }
+
+    .message {
+        font-size: large;
+    }
 </style>
