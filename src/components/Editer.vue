f028a1022284e1e4347086aa31a08aca2fce1a08
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 16:29:05 +0900

    編集画面

--- src/components/Editer.vue
+++ src/components/Editer.vue
@@ -1,9 +1,9 @@
 <template>
     <v-card>
         <v-card-item>
-            <v-text-field variant="underlined" label="タイトル"></v-text-field>
-            <v-card-subtitle class="date">投稿日：2025/08/16</v-card-subtitle>
-            <v-textarea label="記事" counter variant="underlined" rows="11"></v-textarea>
+            <v-text-field variant="underlined" label="タイトル" v-model="blog.title"></v-text-field>
+            <v-card-subtitle class="date">投稿日：{{ blog.created_at }}</v-card-subtitle>
+            <v-textarea label="記事" counter variant="underlined" rows="11" v-model="blog.text"></v-textarea>
             <div class="d-flex justify-end">
                 <v-btn-group>
                     <v-btn>取り消し</v-btn>
@@ -32,6 +32,15 @@
 
 
 <script setup>
+
+
+    import { useRoute, useRouter } from 'vue-router';
+
+    const route = useRoute();
+    const router = useRouter();
+    const detailId = route.params.id;
+
+
     import { ref } from 'vue'
 
     const dialog = ref(false);
@@ -39,6 +48,37 @@
     const save = () => {
         dialog.value = true;
     }
+
+    import { onMounted } from 'vue';
+    import { supabase } from '../../utils/supabase';
+
+    const blog = ref([]);
+
+    class Detail {
+        constructor(props) {
+            this.id = props.id;
+            this.created_at = props.created_at;
+            this.title = props.title;
+            this.text = props.text;
+        }
+    }
+
+    async function getBlog() {
+        const { data: data, error } = await supabase.from('blog').select().eq('id', detailId).single();
+        if (detailId == 0) {
+            const temp = new Detail({ id: 0, created_at: "", title: "", text: "" });
+            blog.value = temp;
+        } else if (error) {
+            router.push('/notfound');
+        } else {
+            const temp = new Detail({ id: data.id, created_at: data.created_at, title: data.title, text: data.text });
+            blog.value = temp;
+        }
+    }
+
+    onMounted(() => {
+        getBlog();
+    })
 
 </script>
 
