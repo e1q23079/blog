1d7dcf90d8108d0be4332fd9a44f069670151f38
e1q23079 <e1q23079@oit.ac.jp>
2025-08-17 22:26:02 +0900

    ルーター修正

--- src/router.ts
+++ src/router.ts
@@ -15,22 +15,15 @@ import Editer from './components/Editer.vue';
 import Forbidden from './components/Forbidden.vue';
 
 import { supabase } from '../utils/supabase';
-
-async function getUser() {
-    const { data: data, error } = await supabase.auth.getUser();
-    if (data['user']) {
-        return true;
-    } else {
-        return false;
-    }
-}
+import { components } from 'vuetify/dist/vuetify-labs.js';
 
 
 const routes = [
     { path: '/', component: Blog },
     { path: '/about', component: About },
     { path: '/login', component: Login },
-    { path: '/editer/:id', component: await getUser() ? Editer : Forbidden },
+    { path: '/editer/:id', component: Editer, meta: { requiresAuth: true } },
+    { path: '/forbidden', component: Forbidden },
     { path: '/detail/:id', component: Detail },
     { path: '/:notFound(.*)', component: Notfound }
 ]
@@ -38,6 +31,19 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        const { data: data, error } = await supabase.auth.getUser();
+        if (!data['user']) {
+            next({ path: '/forbidden' });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 })
 
 export default router;
